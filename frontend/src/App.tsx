02503afd3e53dd07e2cdbd481b637284af0956f8
Benjamin Bamisile <bambobtim@gmail.com>
2024-09-28 23:14:02 -0500

    first commit

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import './global.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <div className='text-red-600 bg-blue-500'>Drip AI</div>
+  )
+}
+
+export default App
---
+++ frontend/src/layouts/layout.tsx
@@ -0,0 +1,12 @@
+type Props = {
+  children: React.ReactNode;
+  showHero?: boolean;
+};
+
+const Layout = () => {
+  return (
+    <div className="bg-grey-500">Drip Ai</div>
+  );
+};
+
+export default Layout;
