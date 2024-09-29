854480e5d2464e92bd646e5f0d2daba0ba939665
Okezie Chiedozie <53785400+okezieuc@users.noreply.github.com>
2024-09-29 14:31:59 +0000

    update response box layout
    Co-authored-by: AMurtadaa <AMurtadaa@users.noreply.github.com>
    Co-authored-by: Manny-14 <Manny-14@users.noreply.github.com>

--- frontend/src/components/Footer.tsx
+++
@@ -1,5 +0,0 @@
-const Footer = () => {
-  return(<div className="text-center">Cook</div>)
-}
-
-export default Footer;
--- frontend/src/components/ResponseBox.tsx
+++ frontend/src/components/ResponseBox.tsx
@@ -6,8 +6,15 @@ type ResponseBoxProps = {
 
 function ResponseBox({ review }: ResponseBoxProps) {
     if (review == null) return <>error</>
-    return <div>
-        {review.rating}
+    return <div className="bg-gray-200 h-full pb-12">
+        <div className="flex flex-col h-full">
+            <div className="grow">
+                {""}
+            </div>
+            <div>
+                Hi
+            </div>
+        </div>
     </div>
 }
 
